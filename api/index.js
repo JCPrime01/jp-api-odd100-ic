f823ddaab161382e3c6d64ecd07577ef6151832d
JCPrime01 <vanzokiller@gmail.com>
2026-05-14 15:20:47 -0300

    update shareCode to MLDQ7L15WCK

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zefg29pw?shareCode=6BW3HUUM0RJ&afp=tapi100' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zefg29pw?shareCode=MLDQ7L15WCK&afp=tapi100' });
     res.end();
   }
